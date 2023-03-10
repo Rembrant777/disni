202496ac8cc9c631096998324de88fefa1035566
nanachi1027 <nanachi1027@163.com>
2023-03-10 09:28:19 +0800

    update test case demo

---
+++ src/test/java/com/ibm/disni/util/EndianUtilsTest.java
@@ -0,0 +1,28 @@
+package com.ibm.disni.util;
+
+import junit.framework.TestCase;
+import org.slf4j.Logger;
+
+/**
+ * Mae
+ * 2023/3/10 9:18 AM
+ */
+public class EndianUtilsTest extends TestCase {
+    private static Logger LOG = DiSNILogger.getLogger();
+
+    public void testSwap1() {
+        LOG.info("#testSwap1");
+    }
+
+    public void testSwap2() {
+        LOG.info("#testSwap2");
+    }
+
+    public void testSwap3() {
+        LOG.info("#testSwap3");
+    }
+
+    public void testSwap4() {
+        LOG.info("#testSwap4");
+    }
+}
