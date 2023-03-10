abe5414a38702aa19ce030b72e067eba8afe0ac5
nanachi1027 <nanachi1027@163.com>
2023-03-10 09:30:13 +0800

    update test case

--- src/test/java/com/ibm/disni/util/EndianUtilsTest.java
+++ src/test/java/com/ibm/disni/util/EndianUtilsTest.java
@@ -12,17 +12,21 @@ public class EndianUtilsTest extends TestCase {
 
     public void testSwap1() {
         LOG.info("#testSwap1");
+
     }
 
     public void testSwap2() {
         LOG.info("#testSwap2");
+
     }
 
     public void testSwap3() {
         LOG.info("#testSwap3");
+
     }
 
     public void testSwap4() {
         LOG.info("#testSwap4");
+        
     }
 }
