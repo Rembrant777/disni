fdab83e63d278d10b2cfd94987fbc6418b56dfd6
nanachi1027 <nanachi1027@163.com>
2023-03-10 16:03:10 +0800

    add test cases

--- src/test/java/com/ibm/disni/util/EndianUtilsTest.java
+++ src/test/java/com/ibm/disni/util/EndianUtilsTest.java
@@ -2,7 +2,6 @@ package com.ibm.disni.util;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import org.slf4j.Logger;
 
 import java.util.Random;
 
@@ -11,10 +10,7 @@ import java.util.Random;
  * 2023/3/10 9:18 AM
  */
 public class EndianUtilsTest extends TestCase {
-    private static Logger LOG = DiSNILogger.getLogger();
-
     public void testSwapShort() {
-        LOG.info("#testSwap1");
         Random rr = new Random();
         short EXPECTED_SHORT = Short.valueOf(String.valueOf(Math.abs(rr.nextInt() % (Short.MAX_VALUE))));
         Assert.assertEquals(EXPECTED_SHORT, EndianUtils.swap(EndianUtils.swap(EXPECTED_SHORT)));
@@ -24,17 +20,25 @@ public class EndianUtilsTest extends TestCase {
     }
 
     public void testSwapChar() {
-        LOG.info("#testSwap2");
-
+        char EXPECTED_CHAR = 'c';
+        Assert.assertEquals(EXPECTED_CHAR, EndianUtils.swap(EndianUtils.swap(EXPECTED_CHAR)));
     }
 
     public void testSwapInt() {
-        LOG.info("#testSwap3");
+        Random rr = new Random();
+        int EXPECTED_INT = Math.abs(rr.nextInt());
+        Assert.assertEquals(EXPECTED_INT, EndianUtils.swap(EndianUtils.swap(EXPECTED_INT)));
 
+        EXPECTED_INT *= -1;
+        Assert.assertEquals(EXPECTED_INT, EndianUtils.swap(EndianUtils.swap(EXPECTED_INT)));
     }
 
     public void testSwapLong() {
-        LOG.info("#testSwap4");
+        Random rr = new Random();
+        long EXPECTED_LONG = Math.abs(rr.nextLong());
+        Assert.assertEquals(EXPECTED_LONG, EndianUtils.swap(EndianUtils.swap(EXPECTED_LONG)));
 
+        EXPECTED_LONG *= -1L;
+        Assert.assertEquals(EXPECTED_LONG, EndianUtils.swap(EndianUtils.swap(EXPECTED_LONG)));
     }
 }
--- src/test/java/com/ibm/disni/util/NetUtilsTest.java
+++ src/test/java/com/ibm/disni/util/NetUtilsTest.java
@@ -1,15 +1,48 @@
 package com.ibm.disni.util;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Mae
  * 2023/3/10 1:06 PM
  */
 public class NetUtilsTest extends TestCase {
-    public void testGetIntIPFromName() {
-        String EXPECTED = "";
-
+    private final static String HOSTNAME = "localhost";
+    public void testGetIntIPFromName() throws UnknownHostException {
+        int rc = NetUtils.getIntIPFromName(HOSTNAME);
+        Assert.assertTrue(rc >= 0);
+    }
+    public void testGetIntIPFromInetAddress() throws UnknownHostException {
+        Assert.assertEquals(NetUtils.getIntIPFromName(HOSTNAME), NetUtils.getIntIPFromInetAddress(InetAddress.getByName(HOSTNAME)));
     }
 
+    public void testGetInetAddressFromIntIp() throws UnknownHostException {
+        int rc = NetUtils.getIntIPFromInetAddress(HOSTNAME);
+        InetAddress inetAddress = NetUtils.getInetAddressFromIntIP(rc);
+        Assert.assertEquals(rc, NetUtils.getIntIPFromInetAddress(inetAddress));
+    }
+
+    public void testShortByteOrder() {
+        short s = Short.MIN_VALUE;
+        Assert.assertEquals(NetUtils.hostToNetworkByteOrder(s), NetUtils.hostToNetworkByteOrder(NetUtils.networkToHostByteOrder(s)));
+    }
+
+    public void testIntByteOrder() {
+        int i = Integer.MAX_VALUE;
+        Assert.assertEquals(NetUtils.hostToNetworkByteOrder(i), NetUtils.hostToNetworkByteOrder(NetUtils.networkToHostByteOrder(i)));
+    }
+
+    public void testLongByteOrder() {
+        long l = Long.MIN_VALUE;
+        Assert.assertEquals(NetUtils.hostToNetworkByteOrder(l), NetUtils.hostToNetworkByteOrder(NetUtils.networkToHostByteOrder(l)));
+    }
+
+    public void testCharByteOrder() {
+        char c = Character.MAX_VALUE;
+        Assert.assertEquals(NetUtils.hostToNetworkByteOrder(c), NetUtils.hostToNetworkByteOrder(NetUtils.networkToHostByteOrder(c)));
+    }
 }
